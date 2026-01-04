593e288701707d1eda769d73f05d5f5161d59a45
Jhofre <darsa.de3@gmail.com>
2026-01-04 15:08:23 -0500

    Base de datos

--- hojadevida/settings.py
+++ hojadevida/settings.py
@@ -82,11 +82,13 @@ WSGI_APPLICATION = 'hojadevida.wsgi.application'
 # DATABASE
 # ===============================
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 # ===============================
 # PASSWORD VALIDATION
